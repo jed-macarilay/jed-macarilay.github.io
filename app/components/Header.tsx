e02bb0f462c67590c7dc843a0c2beb693374a00d
Jedmark Macarilay <jed.macarilay@gmail.com>
2025-05-20 16:36:32 +0800

    Fix issue

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex space-x-6 border-b border-zinc-700 pb-2">
       <NavLink
-        to="/"
+        to="/portfolio"
         className={({ isActive }) =>
           `text-sm font-medium ${isActive ? "text-sm font-medium text-yellow-400 border-b-2 border-yellow-400" : "text-white"}`
         }>Resume</NavLink>
