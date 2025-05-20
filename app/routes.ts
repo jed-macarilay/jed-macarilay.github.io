bc8e0ffb3c05cade4b292e2159f8a1b99219efd9
Jedmark Macarilay <jed.macarilay@gmail.com>
2025-05-20 16:03:41 +0800

    Update routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
+  // index("routes/home.tsx"),
   index("routes/home.tsx"),
+  route("/portfolio", "routes/home.tsx"),
   route("/about", "routes/about.tsx"),
 ] satisfies RouteConfig;
